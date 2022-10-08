5b30e440bfd10ff9c981e2f22a832993d89f121f
Glacy <glacy.kelly3@gmail.com>
2022-10-08 10:24:40 -0300

    Adicionando card component no template

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Entretenimentos } from 'src/app/models/entretenimentos';
+import { Gastronomias } from 'src/app/models/gastronomias';
 import { Hoteis } from 'src/app/models/hoteis';
+import { ENTRETENIMENTOS } from 'src/assets/mocks/mock-entretenimentos';
+import { GASTRONOMIAS } from 'src/assets/mocks/mock-gastronomias';
 import { HOTEIS } from 'src/assets/mocks/mock-hoteis';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   hoteis: Hoteis[] = HOTEIS;
+  gastronomias: Gastronomias[] = GASTRONOMIAS;
+  entretenimentos: Entretenimentos[] = ENTRETENIMENTOS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
