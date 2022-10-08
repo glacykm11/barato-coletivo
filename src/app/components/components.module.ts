7ad387165276362599ed9f237739b61ef4c88e4a
Glacy <glacy.kelly3@gmail.com>
2022-10-08 09:34:28 -0300

    Criação de card component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, CardComponent],
   imports: [
     CommonModule
   ],
