ad3600db471a29ed3847bc59e860241ef97e4cfc
Glacy <glacy.kelly3@gmail.com>
2022-10-10 22:07:46 -0300

    criando página de oferta

---
+++ src/app/pages/oferta/oferta.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OfertaRoutingModule } from './oferta-routing.module';
+import { OfertaComponent } from './oferta.component';
+
+
+@NgModule({
+  declarations: [
+    OfertaComponent
+  ],
+  imports: [
+    CommonModule,
+    OfertaRoutingModule
+  ]
+})
+export class OfertaModule { }
