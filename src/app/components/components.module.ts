bfab84be56ad64892de1ca0c6725174636831eb3
Glacy <glacy.kelly3@gmail.com>
2022-10-12 10:08:15 -0300

    inserindo rota home na logo

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardComponent } from './card/card.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavbarComponent, CardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavbarComponent, CardComponent],
 })
 export class ComponentsModule {}
