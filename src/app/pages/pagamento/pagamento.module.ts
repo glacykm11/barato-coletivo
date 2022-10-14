c2fb95eecf6d6a1fa38aa3d187a8659440d7990e
Glacy <glacy.kelly3@gmail.com>
2022-10-14 20:42:10 -0300

    adicionando reactive forms module

--- src/app/pages/pagamento/pagamento.module.ts
+++ src/app/pages/pagamento/pagamento.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PagamentoRoutingModule } from './pagamento-routing.module';
 import { PagamentoComponent } from './pagamento.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PagamentoComponent],
-  imports: [CommonModule, PagamentoRoutingModule, RouterModule],
+  imports: [
+    CommonModule,
+    PagamentoRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+  ],
 })
 export class PagamentoModule {}
