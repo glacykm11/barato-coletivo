976f4e188a54a5b06b63aa5fc01d5b3006038c46
Glacy <glacy.kelly3@gmail.com>
2022-10-19 20:34:08 -0300

    adicionando novas imagens no mock

--- src/assets/mocks/mock-hoteis.ts
+++ src/assets/mocks/mock-hoteis.ts
@@ -15,7 +15,8 @@ export const HOTEIS: Oferta[] = [
     categoria: 'hotel',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl:
+      'https://media-cdn.tripadvisor.com/media/photo-s/25/04/93/1e/blossom-hotel-houston.jpg',
   },
   {
     id: '11',
@@ -23,7 +24,8 @@ export const HOTEIS: Oferta[] = [
     categoria: 'hotel',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhvrvqk6gYXCSDYmpzRMNMdypsiZ1e68nFOA&usqp=CAU',
   },
   {
     id: '12',
@@ -31,7 +33,8 @@ export const HOTEIS: Oferta[] = [
     categoria: 'hotel',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl:
+      'https://media-cdn.tripadvisor.com/media/photo-s/25/fb/8c/46/hotel-exterior.jpg',
   },
   {
     id: '13',
@@ -39,7 +42,8 @@ export const HOTEIS: Oferta[] = [
     categoria: 'hotel',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '../../../assets/images/hotel-presidente-4s.jpg',
+    imagemUrl:
+      'https://thumbcdn-z.hotelurbano.net/_DDFtBf8ruBMrYb4c1xVXEqzIdU=/444x270/center/middle/filters:quality(40)/https://novo-hu.s3.amazonaws.com/reservas/ota/prod/hotel/529028/enjoy-olimpia-park-resort-002_20191121131749.png',
   },
   {
     id: '14',
@@ -47,7 +51,7 @@ export const HOTEIS: Oferta[] = [
     categoria: 'hotel',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl: 'https://www.ahstatic.com/photos/1276_ho_00_p_1024x768.jpg',
   },
   {
     id: '15',
@@ -55,7 +59,8 @@ export const HOTEIS: Oferta[] = [
     categoria: 'hotel',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl:
+      'https://almundo-com-res.cloudinary.com/image/upload/v1/CVC/destinos/nacional/AL/hotel/HappyHotelPajucara.jpg',
   },
   {
     id: '16',
@@ -63,6 +68,7 @@ export const HOTEIS: Oferta[] = [
     categoria: 'hotel',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl:
+      'https://media-cdn.tripadvisor.com/media/photo-s/21/8d/c3/85/exterior.jpg',
   },
 ];
