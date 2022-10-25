a97196f4afd0d0544308f329e2a1f26d31811e4f
Glacy <glacy.kelly3@gmail.com>
2022-10-24 22:26:20 -0300

    adicionando slider na seção hoteis

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Oferta } from 'src/app/models/oferta';
 import { OfertasService } from 'src/app/shared/services/ofertas/ofertas.service';
@@ -23,5 +23,36 @@ export class HomeComponent implements OnInit {
   comprar(event: Oferta) {
     this.router.navigate([`oferta/${event.id}`]);
     this.ofertasService.setOfertaSelecionada(event);
+  }
+
+  scrollLeft() {
+    var container = document.getElementById('scrolling-wrapper');
+    this.sideScroll(container, 'left', 40, 260, 10);
+  }
+
+  scrollRight() {
+    var container = document.getElementById('scrolling-wrapper');
+    this.sideScroll(container, 'right', 40, 260, 10);
+  }
+
+  sideScroll(
+    element: any,
+    direction: string,
+    speed: number,
+    distance: number,
+    step: number
+  ) {
+    let scrollAmount = 0;
+    var slideTimer = setInterval(function () {
+      if (direction == 'left') {
+        element.scrollLeft -= step;
+      } else {
+        element.scrollLeft += step;
+      }
+      scrollAmount += step;
+      if (scrollAmount >= distance) {
+        window.clearInterval(slideTimer);
+      }
+    }, speed);
   }
 }
