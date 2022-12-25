6f2aa4c107d7d1b98cc5ef668f35f9e8db7f3b69
Glacy <glacy.kelly3@gmail.com>
2022-12-25 10:42:15 -0300

    Adicionando mais informacoes mock-gastronomias

--- src/assets/mocks/mock-gastronomias.ts
+++ src/assets/mocks/mock-gastronomias.ts
@@ -3,34 +3,38 @@ import { Oferta } from 'src/app/models/oferta';
 export const GASTRONOMIAS: Oferta[] = [
   {
     id: '5',
-    nome: 'Gastronomia Long Beach',
+    nome: 'Fogão a lenha',
     categoria: 'gastronomia',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl:
+      'https://blog.serraimperial.com/wp-content/uploads/2020/07/A-gastronomia-%C3%A9-parte-fundamental-de-uma-experi%C3%AAncia-tur%C3%ADstica.png',
   },
   {
     id: '6',
-    nome: 'Gastronomia Long Beach',
+    nome: 'Panela de ferro',
     categoria: 'gastronomia',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl:
+      'https://soubh.uai.com.br/uploads/post/image/11565/main_destaque_Risoto_surf_and_turf_-_cr%C3%A9dito_Victor_Schwaner.jpg',
   },
   {
     id: '7',
-    nome: 'Gastronomia Long Beach',
+    nome: 'Sabores na panela',
     categoria: 'gastronomia',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl:
+      'https://diaonline.ig.com.br/aproveite/wp-content/uploads/2020/04/alta-gastronomia-em-goiania-restaurantes-para-voce-aproveitar.jpeg',
   },
   {
     id: '8',
-    nome: 'Gastronomia Long Beach',
+    nome: 'Recanto de comida caseira',
     categoria: 'gastronomia',
     descricao: '2 diárias em quarto vistas para o mar',
     preco: 100,
-    imagemUrl: '',
+    imagemUrl:
+      'https://media.gazetadopovo.com.br/bomgourmet/2018/04/polvo-768x307-aec66e70.jpg',
   },
 ];
