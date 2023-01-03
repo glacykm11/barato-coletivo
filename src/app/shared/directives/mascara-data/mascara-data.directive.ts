12f44d125cd1100266d23154db1ceeff6950e971
Glacy <glacy.kelly3@gmail.com>
2023-01-02 23:26:34 -0300

    ajustando diretiva de mascara data

--- src/app/shared/directives/mascara-data/mascara-data.directive.ts
+++ src/app/shared/directives/mascara-data/mascara-data.directive.ts
@@ -4,13 +4,30 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[mascaraData]',
 })
 export class MascaraDataDirective {
+  private inputLenght: number;
+
   constructor(private el: ElementRef) {}
 
   @HostListener('input') onKeyPress() {
-    let inputLenght = this.el.nativeElement.value.length;
-
-    if (inputLenght === 2 || inputLenght === 5) {
+    this.inputLenght = this.el.nativeElement.value.length;
+    if (this.inputLenght === 2 || this.inputLenght === 5) {
       this.el.nativeElement.value += '/';
+    }
+  }
+
+  @HostListener('keydown', ['$event']) onKeyDownBackspace(
+    event: KeyboardEvent
+  ) {
+    if (event.key === 'Backspace' && this.inputLenght == 6) {
+      this.el.nativeElement.value =
+        this.el.nativeElement.value.slice(0, -1) + '';
+    }
+    if (
+      (event.key === 'Backspace' || event.key === 'Delete') &&
+      this.inputLenght == 3
+    ) {
+      this.el.nativeElement.value =
+        this.el.nativeElement.value.slice(0, -1) + '';
     }
   }
 }
