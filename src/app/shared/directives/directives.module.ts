908dc238d7175cd07ee3eeb8deb2bae6dfd00c80
Glacy <glacy.kelly3@gmail.com>
2023-01-03 22:19:58 -0300

    adicionando nova diretiva

--- src/app/shared/directives/directives.module.ts
+++ src/app/shared/directives/directives.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MascaraDataDirective } from './mascara-data/mascara-data.directive';
+import { MascaraNumeroCartaoDirective } from './mascara-numero-cartao/mascara-numero-cartao.directive';
 
 @NgModule({
-  declarations: [MascaraDataDirective],
+  declarations: [MascaraDataDirective, MascaraNumeroCartaoDirective],
   imports: [CommonModule],
   exports: [MascaraDataDirective],
 })
