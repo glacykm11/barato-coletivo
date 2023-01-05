a5541d0de5ff0b2be7a56adb9a4b7dbeda3d6523
Glacy <glacy.kelly3@gmail.com>
2023-01-04 23:37:48 -0300

    adicionando máscara número cartão

--- src/app/shared/directives/mascara-numero-cartao/mascara-numero-cartao.directive.ts
+++ src/app/shared/directives/mascara-numero-cartao/mascara-numero-cartao.directive.ts
@@ -6,7 +6,12 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 export class MascaraNumeroCartaoDirective {
   constructor(private el: ElementRef) {}
 
-  @HostListener('input') onKeyPress() {
-    console.log(this.el.nativeElement.value);
+  @HostListener('input', ['$event']) onInputChange(event) {
+    const initalValue = this.el.nativeElement.value;
+    this.el.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
+
+    if (initalValue !== this.el.nativeElement.value) {
+      event.stopPropagation();
+    }
   }
 }
