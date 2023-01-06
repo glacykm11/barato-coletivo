8e4f28adcc65fae341c76858064eba0f808ed79d
Glacy <glacy.kelly3@gmail.com>
2023-01-05 22:53:38 -0300

    adicionando MascaraNumeroCartaoDirective

--- src/app/shared/directives/mascara-numero-cartao/mascara-numero-cartao.directive.ts
+++ src/app/shared/directives/mascara-numero-cartao/mascara-numero-cartao.directive.ts
@@ -4,14 +4,24 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[mascaraNumeroCartao]',
 })
 export class MascaraNumeroCartaoDirective {
+  private inputLenght: number;
   constructor(private el: ElementRef) {}
 
-  @HostListener('input', ['$event']) onInputChange(event) {
+  @HostListener('input', ['$event']) onInputChange(event: Event) {
     const initalValue = this.el.nativeElement.value;
-    this.el.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
+    this.el.nativeElement.value = initalValue.replace(/[A-z]*/g, '');
 
     if (initalValue !== this.el.nativeElement.value) {
       event.stopPropagation();
+    }
+
+    this.inputLenght = this.el.nativeElement.value.length;
+    if (
+      this.inputLenght === 4 ||
+      this.inputLenght === 9 ||
+      this.inputLenght === 14
+    ) {
+      this.el.nativeElement.value += ' ';
     }
   }
 }
