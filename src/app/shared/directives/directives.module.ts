5219078ed93e09f058981386caa2afebc2bf3dd3
Glacy <glacy.kelly3@gmail.com>
2023-01-05 23:00:22 -0300

    Adicionando MascaraNomeDirective

--- src/app/shared/directives/directives.module.ts
+++ src/app/shared/directives/directives.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { MascaraDataDirective } from './mascara-data/mascara-data.directive';
 import { MascaraNumeroCartaoDirective } from './mascara-numero-cartao/mascara-numero-cartao.directive';
 import { MascaraCodigoSegurancaDirective } from './mascara-codigo-seguranca/mascara-codigo-seguranca.directive';
+import { MascaraNomeDirective } from './mascara-nome/mascara-nome.directive';
 
 @NgModule({
   declarations: [
     MascaraDataDirective,
     MascaraNumeroCartaoDirective,
     MascaraCodigoSegurancaDirective,
+    MascaraNomeDirective,
   ],
   imports: [CommonModule],
   exports: [
     MascaraDataDirective,
     MascaraNumeroCartaoDirective,
     MascaraCodigoSegurancaDirective,
+    MascaraNomeDirective,
   ],
 })
 export class DirectivesModule {}
