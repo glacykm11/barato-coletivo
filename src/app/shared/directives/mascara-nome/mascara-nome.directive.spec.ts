89cf4f8f6c603513e2729cdae2620a3a9fe66ebf
Glacy <glacy.kelly3@gmail.com>
2023-01-05 23:01:11 -0300

    adicionando mascaraNomeDirective

---
+++ src/app/shared/directives/mascara-nome/mascara-nome.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MascaraNomeDirective } from './mascara-nome.directive';
+
+describe('MascaraNomeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MascaraNomeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
