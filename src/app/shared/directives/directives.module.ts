32de49b0792a575430d8afb0e994ea0e60fee860
Glacy <glacy.kelly3@gmail.com>
2023-01-05 22:50:55 -0300

    adicionando novas diretivas

--- src/app/shared/directives/directives.module.ts
+++ src/app/shared/directives/directives.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MascaraDataDirective } from './mascara-data/mascara-data.directive';
 import { MascaraNumeroCartaoDirective } from './mascara-numero-cartao/mascara-numero-cartao.directive';
+import { MascaraCodigoSegurancaDirective } from './mascara-codigo-seguranca/mascara-codigo-seguranca.directive';
 
 @NgModule({
-  declarations: [MascaraDataDirective, MascaraNumeroCartaoDirective],
+  declarations: [
+    MascaraDataDirective,
+    MascaraNumeroCartaoDirective,
+    MascaraCodigoSegurancaDirective,
+  ],
   imports: [CommonModule],
-  exports: [MascaraDataDirective, MascaraNumeroCartaoDirective],
+  exports: [
+    MascaraDataDirective,
+    MascaraNumeroCartaoDirective,
+    MascaraCodigoSegurancaDirective,
+  ],
 })
 export class DirectivesModule {}
