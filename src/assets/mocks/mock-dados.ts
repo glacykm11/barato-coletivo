a9e44476d8f42a8d6a4c3b265b7d568d2fdbdcc7
Glacy <glacy.kelly3@gmail.com>
2023-01-06 21:52:35 -0300

    adicionando mock dados gerais

---
+++ src/assets/mocks/mock-dados.ts
@@ -0,0 +1,218 @@
+import { Oferta } from 'src/app/models/oferta';
+
+export const ENTRETENIMENTOS: Oferta[] = [
+  {
+    id: '1',
+    nome: 'Cinemark',
+    categoria: 'entretenimento',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 200,
+    imagemUrl:
+      'https://www.exibidor.com.br/fotos/noticias/notundefined_1657221690.jpg',
+  },
+  {
+    id: '2',
+    nome: 'UCI Kinoplex',
+    categoria: 'entretenimento',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 320,
+    imagemUrl:
+      'https://s2.glbimg.com/ZOj1qWV5DnyzKKSDuKSe9SHw6Us=/512x320/smart/e.glbimg.com/og/ed/f/original/2014/03/04/kinoplex.jpg',
+  },
+  {
+    id: '3',
+    nome: 'Box Cinemas',
+    categoria: 'entretenimento',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 150,
+    imagemUrl:
+      'http://4.bp.blogspot.com/-6MZF5mnkrfE/T58Z0FcBf1I/AAAAAAAABXw/RVltjPyiIOc/s1600/loja-boxcinemas.jpg',
+  },
+  {
+    id: '4',
+    nome: 'MASP',
+    categoria: 'entretenimento',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 350,
+    imagemUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/9/9d/MASP_Brazil.jpg',
+  },
+  {
+    id: '5',
+    nome: 'Museu do Amanhã',
+    categoria: 'entretenimento',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 130,
+    imagemUrl:
+      'https://www.aberje.com.br/wp-content/uploads/2022/03/Museu-do-Amanha_Divulgacao-scaled.jpg',
+  },
+  {
+    id: '6',
+    nome: 'Museu do Reggae',
+    categoria: 'entretenimento',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 180,
+    imagemUrl:
+      'https://catracalivre.com.br/wp-content/uploads/2017/12/03504_museu_do_reggae_2501744091266536436.jpg',
+  },
+  {
+    id: '7',
+    nome: 'Museu Imperial',
+    categoria: 'entretenimento',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 160,
+    imagemUrl:
+      'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/f4/3e/ed/city-tour-rio.jpg?w=1200&h=-1&s=1',
+  },
+  {
+    id: '8',
+    nome: 'Parque Lage',
+    categoria: 'entretenimento',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 150,
+    imagemUrl:
+      'https://rio.temporadacopacabana.com.br/wp-content/uploads/2020/10/31-parque-lage1-720x430.jpg',
+  },
+  {
+    id: '9',
+    nome: 'Fogão a lenha',
+    categoria: 'gastronomia',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 150,
+    imagemUrl:
+      'https://blog.serraimperial.com/wp-content/uploads/2020/07/A-gastronomia-%C3%A9-parte-fundamental-de-uma-experi%C3%AAncia-tur%C3%ADstica.png',
+  },
+  {
+    id: '10',
+    nome: 'Panela de ferro',
+    categoria: 'gastronomia',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 120,
+    imagemUrl:
+      'https://soubh.uai.com.br/uploads/post/image/11565/main_destaque_Risoto_surf_and_turf_-_cr%C3%A9dito_Victor_Schwaner.jpg',
+  },
+  {
+    id: '11',
+    nome: 'Sabores na panela',
+    categoria: 'gastronomia',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 700,
+    imagemUrl:
+      'https://diaonline.ig.com.br/aproveite/wp-content/uploads/2020/04/alta-gastronomia-em-goiania-restaurantes-para-voce-aproveitar.jpeg',
+  },
+  {
+    id: '12',
+    nome: 'Recanto de comida caseira',
+    categoria: 'gastronomia',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 600,
+    imagemUrl:
+      'https://media.gazetadopovo.com.br/bomgourmet/2018/04/polvo-768x307-aec66e70.jpg',
+  },
+  {
+    id: '13',
+    nome: 'Fogão a lenha',
+    categoria: 'gastronomia',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 200,
+    imagemUrl:
+      'https://blog.serraimperial.com/wp-content/uploads/2020/07/A-gastronomia-%C3%A9-parte-fundamental-de-uma-experi%C3%AAncia-tur%C3%ADstica.png',
+  },
+  {
+    id: '14',
+    nome: 'Panela de ferro',
+    categoria: 'gastronomia',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 500,
+    imagemUrl:
+      'https://soubh.uai.com.br/uploads/post/image/11565/main_destaque_Risoto_surf_and_turf_-_cr%C3%A9dito_Victor_Schwaner.jpg',
+  },
+  {
+    id: '15',
+    nome: 'Sabores na panela',
+    categoria: 'gastronomia',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 335,
+    imagemUrl:
+      'https://diaonline.ig.com.br/aproveite/wp-content/uploads/2020/04/alta-gastronomia-em-goiania-restaurantes-para-voce-aproveitar.jpeg',
+  },
+  {
+    id: '16',
+    nome: 'Recanto de comida caseira',
+    categoria: 'gastronomia',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 220,
+    imagemUrl:
+      'https://media.gazetadopovo.com.br/bomgourmet/2018/04/polvo-768x307-aec66e70.jpg',
+  },
+  {
+    id: '17',
+    nome: 'Hotel Long Beach',
+    categoria: 'hotel',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 180,
+    imagemUrl: '../../../assets/images/hotel-presidente-4s.jpg',
+  },
+  {
+    id: '18',
+    nome: 'Hotel Mirante',
+    categoria: 'hotel',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 700,
+    imagemUrl:
+      'https://media-cdn.tripadvisor.com/media/photo-s/25/04/93/1e/blossom-hotel-houston.jpg',
+  },
+  {
+    id: '19',
+    nome: 'Hotel Recanto Divino',
+    categoria: 'hotel',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 600,
+    imagemUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhvrvqk6gYXCSDYmpzRMNMdypsiZ1e68nFOA&usqp=CAU',
+  },
+  {
+    id: '20',
+    nome: 'Hotel Mineiro',
+    categoria: 'hotel',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 500,
+    imagemUrl:
+      'https://media-cdn.tripadvisor.com/media/photo-s/25/fb/8c/46/hotel-exterior.jpg',
+  },
+  {
+    id: '21',
+    nome: 'Hotel Sol Nascente',
+    categoria: 'hotel',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 110,
+    imagemUrl:
+      'https://thumbcdn-z.hotelurbano.net/_DDFtBf8ruBMrYb4c1xVXEqzIdU=/444x270/center/middle/filters:quality(40)/https://novo-hu.s3.amazonaws.com/reservas/ota/prod/hotel/529028/enjoy-olimpia-park-resort-002_20191121131749.png',
+  },
+  {
+    id: '22',
+    nome: 'Hotel Varandas',
+    categoria: 'hotel',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 150,
+    imagemUrl: 'https://www.ahstatic.com/photos/1276_ho_00_p_1024x768.jpg',
+  },
+  {
+    id: '23',
+    nome: 'Hotel Vila Estrelada',
+    categoria: 'hotel',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 120,
+    imagemUrl:
+      'https://almundo-com-res.cloudinary.com/image/upload/v1/CVC/destinos/nacional/AL/hotel/HappyHotelPajucara.jpg',
+  },
+  {
+    id: '24',
+    nome: 'Óasis Hotel',
+    categoria: 'hotel',
+    descricao: '2 diárias em quarto vistas para o mar',
+    preco: 140,
+    imagemUrl:
+      'https://media-cdn.tripadvisor.com/media/photo-s/21/8d/c3/85/exterior.jpg',
+  },
+];
